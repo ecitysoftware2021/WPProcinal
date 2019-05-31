91cbc9d661377c15aa6d9bc7b6211be8228ced02
dcordobag <32066181+dcordobag@users.noreply.github.com>
2019-05-31 14:11:49 -0500

    Nuevo formulario
    Se agrega formulario de pago exitoso

---
+++ WPProcinal/Forms/FrmFinalTransaction.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPProcinal.Classes;
+
+namespace WPProcinal.Forms
+{
+    /// <summary>
+    /// Interaction logic for FrmFinalTransaction.xaml
+    /// </summary>
+    public partial class FrmFinalTransaction : Window
+    {
+        public FrmFinalTransaction()
+        {
+            InitializeComponent();
+            Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                Dispatcher.BeginInvoke((Action)delegate
+                {
+                    Utilities.GoToInicial(this);
+                });
+            });
+        }
+
+        private void BtnConsult_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Utilities.GoToInicial(this);
+        }
+    }
+}
